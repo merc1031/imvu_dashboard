61bbae693a576109f166d704f7c53e1cb4065916
Matt Chun-Lum <mchunlum@imvu.com>
2012-11-09 00:05:35 -0800

    color switching works for push widget

--- jobs/push_status.rb
+++ jobs/push_status.rb
@@ -1,24 +1,45 @@
 require 'net/http'
 require 'json'
 
-def get_push_status (source)
+def get_build_metadata (key)
     base_uri = URI.parse('http://engprocess.corp.imvu.com/buildslaves/api/build_metadata.php');
-    base_uri.query = URI.encode_www_form({ :action => 'get', :key => "#{source}_push_status"})
+    base_uri.query = URI.encode_www_form({ :action => 'get', :key => key })
     response = Net::HTTP.get_response(base_uri)
     if response.is_a?(Net::HTTPSuccess)
-        body = JSON.parse(response.body)
-        data = JSON.parse(body['value'])
-        data['start'] = data['start'][10, 8]
+        return JSON.parse(response.body)
+    end
+    return nil
+end
+
+def get_push_status (source)
+    raw = get_build_metadata "#{source}_push_status"
+    unless raw.nil?
+        data = JSON.parse(raw['value'])
+        rev_in_production = get_current_revision source
+        data['rev_in_production'] = rev_in_production
+        data['start'] = data['start'][11, 8]
         data['rev'] = data['rev'][0, 8]
+        data['end'] = data['end'][10, 8] unless data['end'].nil?
+
         if data['end'].nil?
             data['status'] = -1
+        elsif rev_in_production == data['rev']
+            data['status'] = 1
         else
-            data['end'] = data['end'][10, 8]
-            data['status'] = body['success']
+            data['status'] = 0
         end
+
         return data
     end
     return {}
+end
+
+def get_current_revision (source)
+    data = get_build_metadata "#{source}_rev_in_production"
+    unless data.nil?
+        return data['value'][0, 8]
+    end
+    return 'UNKNOWN'
 end
 
 def get_all_push_statuses
--- widgets/push/push.coffee
+++ widgets/push/push.coffee
@@ -1,5 +1,9 @@
 class Dashing.Push extends Dashing.Widget
 
+    in_progress = '#eb9c3c'
+    failure     = '#dc5945'
+    success     = '#96bf48'
+
     onData: (data) ->
         if data.statuses
             @updateStatus key, d for key, d of data.statuses
@@ -10,10 +14,18 @@ class Dashing.Push extends Dashing.Widget
             @appendNew key, data
         $elem = $(@node).find(status_key)
         $elem.find('.type').text(key)
-        $elem.find('.current h2').text('todo')
+        $elem.find('.current h2').text(data.rev_in_production)
         $elem.find('.rising h2').text(data.rev)
         $elem.find('.user h2').text(data.user)
         $elem.find('.time h2').text(data.start)
+
+        $elem.find('.rising').toggle(data.status != 1)
+        if data.status < 0
+            $elem.find('div').css('background-color', in_progress)
+        else if data.status == 0
+            $elem.find('div').css('background-color', failure)
+        else
+            $elem.find('div').css('background-color', success)
 
     appendNew: (key) ->
         $elem = $(@node).find('.template li').clone()
