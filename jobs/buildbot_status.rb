bd304027ee5e6da24618135054c42ed38e0d7b60
Matt Chun-Lum <mchunlum@imvu.com>
2012-11-10 14:55:24 -0800

    refactor buildbot code into a module, rename job

---
+++ jobs/buildbot_status.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/buildbot'
+
+SCHEDULER.every '20s', :first_in => 0 do |job|
+  send_event('web_buildbot', Buildbot.get_build_data('webbuildbot'))
+  send_event('web_hypo_buildbot', Buildbot.get_build_data('hypothesisbuildbot.corp.imvu.com:8010'))
+end
