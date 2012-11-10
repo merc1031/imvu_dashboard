6766ae9fc935e9f540d68202f09e65483f7c0b71
Leonidas Loucas <merc1031@gmail.com>
2012-11-10 05:53:39 -0800

    work in progress

--- jobs/buildbot.rb
+++ jobs/buildbot.rb
@@ -2,6 +2,22 @@ require 'net/http'
 require 'json'
 require 'date'
 
+def get_builder_state(root_url, slave)
+    base_uri = URI.parse("http://#{root_url}/json/builders/#{slave}/builds?select=-1&select=-2");
+    response = Net::HTTP.get_response(base_uri)
+    if response.is_a?(Net::HTTPSuccess)
+        JSON.parse(response.body)
+    end
+end
+
+def get_builders(root_url)
+    base_uri = URI.parse("http://#{root_url}/json/builders/");
+    response = Net::HTTP.get_response(base_uri)
+    if response.is_a?(Net::HTTPSuccess)
+        JSON.parse(response.body)
+    end
+end
+
 def get_current_build(root_url)
     base_uri = URI.parse("http://#{root_url}/json/builders/aggregator/builds/-1/");
     response = Net::HTTP.get_response(base_uri)
@@ -15,6 +31,55 @@ def get_aggregator(root_url)
     response = Net::HTTP.get_response(base_uri)
     if response.is_a?(Net::HTTPSuccess)
         JSON.parse(response.body)
+    end
+end
+
+def get_builders_data(buildbot)
+    build_data = get_builders buildbot
+    success_count = 0
+    failure_count = 0
+    warnings_count = 0
+    skipped_count = 0
+    exception_count = 0
+    retry_count = 0
+    unless build_data.nil?
+        build_data.each { |n, v|
+            state = get_builder_state buildbot, n
+            builder_state = state['-1']['results']
+            #if state['-1']['result'].nil?
+            #    builder_state = state['-2']['results']
+            #end
+
+            if builder_state == 0 
+                success_count += 1
+            end
+            if builder_state == 2
+                failure_count += 1
+            end
+            if builder_state == 1
+                warnings_count += 1
+            end
+            if builder_state == 3
+                skipped_count += 1
+            end
+            if builder_state == 4
+                exception_count += 1
+            end
+            if builder_state == 5
+                retry_count += 1
+            end
+        }
+        data =  
+        {
+            :success => success_count,
+            :failed => failure_count,
+            :warnings => warnings_count,
+            :skipped => skipped_count,
+            :exception => exception_count,
+            :retry => retry_count
+        }
+    else
+        {}
     end
 end
 
@@ -54,6 +119,7 @@ def extract_times(data)
 end
 
 SCHEDULER.every '20s', :first_in => 0 do |job|
-  send_event('web_buildbot', get_build_data('webbuildbot'))
-  send_event('web_hypo_buildbot', get_build_data('hypothesisbuildbot.corp.imvu.com:8010'))
+  send_event('web_hypo_buildbot', { :status => get_builders_data('hypothesisbuildbot.corp.imvu.com:8010') } )
+    #send_event('web_buildbot', get_build_data('webbuildbot'))
+  #send_event('web_hypo_buildbot', get_build_data('hypothesisbuildbot.corp.imvu.com:8010'))
 end
--- widgets/buildbot/buildbot.coffee
+++ widgets/buildbot/buildbot.coffee
@@ -1,3 +1,13 @@
 class Dashing.Buildbot extends Dashing.Widget
     ready: ->
-        @set('current_build', '12345')
+
+    onData: (data) ->
+        @updateStatus data.status
+
+    updateStatus: (status) ->
+        @set('failed', status.failed)
+        @set('success', status.success)
+        @set('warnings', status.warnings)
+        @set('skipped', status.skipped)
+        @set('exception', status.exception)
+        @set('retry', status.retry)
