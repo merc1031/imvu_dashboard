b00635931907d8e323c92f49fdbac35918355c0a
Matt Chun-Lum <mchunlum@imvu.com>
2012-11-10 20:32:37 -0800

    sanity commit, more layout tweaks for buildbot widgets, still need to fix css styling

--- lib/buildbot.rb
+++ lib/buildbot.rb
@@ -3,12 +3,21 @@ require 'json'
 require 'date'
 
 module Buildbot
-    def self.get_current_build(root_url)
-        base_uri = URI.parse("http://#{root_url}/json/builders/aggregator/builds/-1/");
+
+    def self.get_build(root_url, build)
+        base_uri = URI.parse("http://#{root_url}/json/builders/aggregator/builds/#{build}/");
         response = Net::HTTP.get_response(base_uri)
         if response.is_a?(Net::HTTPSuccess)
             JSON.parse(response.body)
         end
+    end
+
+    def self.get_current_build(root_url)
+        get_build(root_url, -1)
+    end
+
+    def self.get_last_build(root_url)
+        get_build(root_url, -2)
     end
 
     def self.get_aggregator(root_url)
@@ -20,16 +29,18 @@ module Buildbot
     end
 
     def self.get_build_data(buildbot)
-        build_data = get_current_build buildbot
-        aggregator_data = get_aggregator buildbot
+        build_data = get_current_build(buildbot)
+        aggregator_data = get_aggregator(buildbot)
+        last_build_data = get_last_build(buildbot)
         unless build_data.nil? or aggregator_data.nil?
             times = extract_times build_data
             {
                 :revisions => extract_revisions(build_data),
-                :state => extract_state(build_data, aggregator_data),
                 :start => times[0],
                 :end => times[1],
-                :queue => aggregator_data['pendingBuilds'] || 0
+                :queue => aggregator_data['pendingBuilds'] || 0,
+                :state => extract_state(build_data, aggregator_data),
+                :last_state => extract_state(last_build_data)
             }
         else
             {}
@@ -40,7 +51,7 @@ module Buildbot
         data['sourceStamp']['changes'].collect { |change| { :user => change['who'], :rev => change['rev'][0, 8] } }
     end
 
-    def self.extract_state(build_data, aggregator_data)
+    def self.extract_state(build_data, aggregator_data={ :state => 'idle' })
         if build_data['text'].include? 'failed'
             'failure'
         elsif build_data['text'].include? 'exception'
