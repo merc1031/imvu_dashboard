e21f70bce978a97175b6e2af0f827c3de7be5f94
Leonidas Loucas <merc1031@gmail.com>
2012-11-11 23:43:15 -0800

    finally getting data out of caltrain

--- lib/caltrain.rb
+++ lib/caltrain.rb
@@ -3,9 +3,12 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 require 'date'
+require 'time'
 
 module Caltrain
 
+    @@noonSwitchOver = '151'
+    @@badHeads = ['Zone', 'Northbound Train No.']
     def self.get_caltrain_table()
         base_uri = URI.parse("http://www.caltrain.com/schedules/weekdaytimetable.html")
         response = Net::HTTP.get_response(base_uri)
@@ -20,32 +23,126 @@ module Caltrain
         end
     end
 
-    def self.extract_stop(data)
-        northRow = extract_one_stop data, :northbound
-        southRow = extract_one_stop data, :southbound
-        data = 
+    def self.filter_header(row)
+        row.delete_if { |x| @@badHeads.include?(x.gsub /[[:space:]]/, ' ') }
+    end
+
+    def self.extract_header(data)
+        rows = data[:northbound].search('tr')
+        row = filter_header(nodeset_to_timelist rows[0], 'th')
+    end
+
+    def self.noon_train_index(header)
+        endIndex = 0
+        header.each_with_index do |item, index|
+            puts item
+            if item.eql? @@noonSwitchOver
+                endIndex = index
+                break
+            end
+        end
+        data = endIndex
+    end
+
+    def self.add_metadata_to( item, header_item)
+        departureMayBeDelayed = false
+        type = nil
+
+        if header_item.include? '#'
+            header_item = header_item.delete '#'
+            departureMayBeDelayed = true
+        end
+
+        if Integer(header_item) >= 300
+            type = "Baby Bullet"
+        elsif Integer(header_item) >= 200
+            type = "Limited-stop"
+        elsif Integer(header_item) >= 100
+            type = "Local"
+        end
+        data =
         {
-            :northrow => nodeset_to_inner_text(northRow),
-            :southrow => nodeset_to_inner_text(southRow)
+            :time => item,
+            :type => type,
+            :number => header_item,
+            :departureMayBeDelayed => departureMayBeDelayed
         }
     end
 
-    def self.extract_one_stop(data, stop)
-        rows = data[stop].search('tr')
+    def self.add_metadata(timelist, header)
+        timelist.map.with_index { |x,i| add_metadata_to x, header[i] }
+    end
+
+    def self.extract_stop(data, stop)
+        northRow = extract_one_stop data, :northbound, stop
+        southRow = extract_one_stop data, :southbound, stop
+        header = extract_header data
+        noonIndex = noon_train_index header
+        data = 
+        {
+            :northrow => add_metadata(map_time(nodeset_to_timelist(northRow), noonIndex), header),
+            :southrow => add_metadata(map_time(nodeset_to_timelist(southRow), noonIndex), header)
+        }
+    end
+
+    def self.extract_one_stop(data, route, stop)
+        rows = data[route].search('tr')
         stoprow = nil 
         rows.each do |row|
             entries = row.search('th')
             name = entries[1].inner_text
             name = name.gsub /[[:space:]]/, ' '
-            if name.include? "Mountain View"
+            if name.include? stop 
                 stoprow = row
                 break
             end
         end
         ret = stoprow
     end
+   
+    def self.filter_time(item, index, noonIndex)
+        parsedTime = '-'
+        if ! item.eql? '-'
+            if index >= noonIndex
+                parsedTime = (Time.parse(item) + (12 * 60 * 60)).strftime('%H:%M') 
+            else
+                parsedTime = Time.parse(item).strftime('%H:%M')
+            end
+        end
+        ret = parsedTime
+    end
 
-    def self.nodeset_to_inner_text(nodeset)
-        nodeset.search('td').collect { |x| x.inner_text }
+    def self.map_time(timelist, noonIndex)
+        am = true
+        y = nil
+        timelist.each_with_index do |x,i|
+            if ! x.eql? '-'
+                if !y.nil?
+                    #this is sloppy and crappy. caltrain doesnt list 24h time and no indication of when it switches over
+                    #and trains arent all in perfect order....
+                    if Time.parse(x) - Time.parse(y) < 8 and am
+                        am = false
+                    elsif Time.parse(x) - Time.parse(y) < 8 and !am
+                        am = true
+                    end
+                end
+                if !am
+                    timelist[i] = (Time.parse(x) + (12 * 60 * 60)).strftime('%H:%M')
+                else
+                    timelist[i] = Time.parse(x).strftime('%H:%M')
+                end
+                y = x
+            end
+        end
+        ret = timelist 
+        #puts timelist
+        #temp = timelist.map.with_index { |x,i| filter_time x, i, noonIndex }
+        #ret = temp.delete_if { |x| x.eql? '-' }
+        #d = timelist
+    end
+
+    def self.nodeset_to_timelist(nodeset, search='td')
+        #nodeset.search('td').collect { |x| x.inner_text.strip }
+        nodeset.search(search).collect { |x| x.inner_text.strip }
     end
 end
